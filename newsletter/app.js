6f6ec9f8576581620dd5e76a718e956e2cb1e032
rohitbhaskar <bhaskar.rohit.work@gmail.com>
2018-06-16 02:00:38 +0530

    finalized section JSON object structure; finalized background color ng-style

--- newsletter/app.js
+++ newsletter/app.js
@@ -13,9 +13,9 @@ app.controller('MainCtrl', function($scope) {
     		name: "competitions",
 
     		// Header
-    		header_img_link: "",
+    		header_img_url: "",
     		header_img_name: "",
-    		header_bg_color: "",
+    		header_bg_color : function() {return "#00897B";},
     		header_text: "Competitions",
 
     		// Sub-Header
@@ -28,67 +28,15 @@ app.controller('MainCtrl', function($scope) {
     				description: "",
     				link_url: "",
     				link_text: ""
-    			}
-    		]
-    	}
-    ];
-
-
-    $scope.test_sections=[
-    	{
-    		// General Details
-    		name: "competitions",
-
-    		// Header
-    		header_img_url: "https://i.imgur.com/flb3wf7.jpg",
-    		header_img_name: "",
-    		header_bg_gradient : function() {
-				  return {'background':'linear-gradient(to right, #8E2DE2, #4A00E0)'};
-				},
-    		header_text: "Competitions",
-
-    		// Sub-Header
-    		subheader_text: "",
-
-    		// Content (Body)
-    		content: [
-    			{
-    				header: "Material Design Email Template",
-    				description: "This is an html email template based on Google's Material Design. This template has been tested on 33 major email clients using Litmus Builder. Feel free to use this for whatever purpose.",
-    				link_url: "",
-    				link_text: ""
     			},
     			{
-    				header: "Related Other Template",
-    				description: "Related other deisciption",
+    				header: "",
+    				description: "",
     				link_url: "",
     				link_text: ""
     			}
     		]
     	},
-    	{
-    		// General Details
-    		name: "news",
-
-    		// Header
-    		header_img_url: "https://i.imgur.com/flb3wf7.jpg",
-    		header_img_name: "",
-    		header_bg_color: "",
-    		header_text: "News",
-
-    		// Sub-Header
-    		subheader_text: "",
-
-    		// Content (Body)
-    		content: [
-    			{
-    				header: "Some Other Template",
-    				description: "Some other deisciption",
-    				link_url: "",
-    				link_text: ""
-    			}
-    		]
-    	}
     ]
 
 });
