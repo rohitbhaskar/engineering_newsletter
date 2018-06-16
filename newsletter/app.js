48957b11fac121e949f5d14bc0108de997a54c3c
rohitbhaskar <bhaskar.rohit.work@gmail.com>
2018-06-17 02:26:57 +0530

    added emojis to each heading text

--- newsletter/app.js
+++ newsletter/app.js
@@ -25,7 +25,8 @@ app.controller('MainCtrl', function($scope) {
     	archive_link:"",
 
     	// logo
-    	logo_link:"https://i.imgur.com/fZXmFqT.png",
+    	logo_image:"",
+    	logo_link:"",
     	logo_alt_text:"The Indian Engineer Logo"
     }
 
@@ -36,9 +37,9 @@ app.controller('MainCtrl', function($scope) {
 	$scope.footer={
 		icons:[
 			{
-				image:"",
+				image_link:"https://i.imgur.com/ApMgbF2.png",
 				image_alt_text:"Github | The Indian Engineer",
-				url:"https://github.com/rohitbhaskar/engineering_newsletter"
+				link:"https://github.com/rohitbhaskar/engineering_newsletter"
 			},
 		]
 	}
@@ -63,13 +64,15 @@ app.controller('MainCtrl', function($scope) {
     		content: [
     			{
     				header: "Element14 | Design for a cause challenge",
-    				description: "Free Development Kit to hack around, showcase your talent and help the world. Do you need anything more exciting than that!? (Idead Submission deadline:20th June '18)",
+    				header_icon:"🎁",
+    				description: "Free Development Kit to hack around, showcase your talent and help the world. Do you need anything more exciting than that!? (Idea Submission deadline: 20th June '18)",
     				link_url: "http://bit.ly/element14_competition",
     				link_text: "element14.com"
     			},
     			{
     				header: "HackerEarth | World Music Hackathon",
-    				description: "Software hackathon with themes revolving around using music(Prototype submission deadline : 1st July)",
+    				header_icon:"🎵",
+    				description: "Software hackathon with themes revolving around using music(Prototype submission deadline: 1st July)",
     				link_url: "http://bit.ly/world-music-hackathon",
     				link_text: "hackerearth.com"
     			}
@@ -96,9 +99,9 @@ app.controller('MainCtrl', function($scope) {
     		// Content (Body)
     		content: [
     			{
-    				header: "Marketing Coordinator | Zaya Learning Labs",
-    				header_icon:"",
-    				description: "No stipend, but its a startup, so you'll learn a lot (Not like interns get a huge amount of money anyway ;p)",
+    				header: "Marketing Co-ordinator | Zaya Learning Labs",
+    				header_icon:"👨‍💼",
+    				description: "No stipend, but its a startup, so you'll learn a lot (Not like interns get a lot of money anyway ;p)",
     				link_url: "http://bit.ly/angellist-marketing-internship",
     				link_text: "angel.co",
     				link_icon: ""
@@ -126,12 +129,14 @@ app.controller('MainCtrl', function($scope) {
     		content: [
     			{
     				header: "Net neutrality dead in the states?",
-    				description: "The fallout of open and equal internet",
+    				header_icon:"🌐",
+    				description: "The fallout from the end of open and equal internet",
     				link_url: "http://bit.ly/fcc-net-neutrality",
     				link_text: "cnet.com"
     			},
     			{
     				header: "Bitcoin falls 10% after a Korean exchange got hacked",
+    				header_icon:"📉",
     				description: "Bitcoin has been a roller coaster ride that just gets thrilling every month :P",
     				link_url: "http://bit.ly/bitcoin-crash-10",
     				link_text: "quartz"
@@ -159,24 +164,28 @@ app.controller('MainCtrl', function($scope) {
     		content: [
     			{
     				header: "Learn Javascript by playing games",
+    				header_icon:"🎮",
     				description: "Do you ever get bored learning javascript the same old way? Learn it by playing a command line game",
     				link_url: "http://bit.ly/github-javascript-game",
     				link_text: "github.com"
     			},
     			{
-    				header: "Amazing tech talks curated by the tech community",
+    				header: "Amazing tech talks curated by the community",
+    				header_icon:"📢",
     				description: "",
     				link_url: "http://bit.ly/awesome-tech-talks",
     				link_text: "awesometalks"
     			},
     			{
-    				header: "DNS over HTTP explained through cartoons",
-    				description: "Like comic strips? Learn more about the working of the connected world as a Firefox dev explaining DNS over HTTPS (DoH) using...well...cartoons :)",
+    				header: "DNS over HTTPS explained through cartoons",
+    				header_icon:"🔐",
+    				description: "Like comic strips? Learn more about the working of the connected world as a Firefox dev explains DNS over HTTPS (DoH) using...well...cartoons :)",
     				link_url: "http://bit.ly/dns-https",
     				link_text: "mozilla.org"
     			},
     			{
     				header: "Learn about convolutional neural networks in 2 minutes",
+    				header_icon:"🕸",
     				description: "",
     				link_url: "http://bit.ly/sravjti-cnn",
     				link_text: "sra blog"
@@ -204,42 +213,49 @@ app.controller('MainCtrl', function($scope) {
     		content: [
     			{
     				header: "The future terminator just got another upgrade",
+    				header_icon:"🤖",
     				description: "Smart robotic skin that heals itself. Even if you shoot it full of holes. (Yeah you read that right)",
     				link_url: "http://bit.ly/soft-robotic-skin",
     				link_text: "futurism.com"
     			},
     			{
     				header: "5D Glass Discs Store 3000x More Info Than CDs",
+    				header_icon:"💿",
     				description: "",
     				link_url: "http://bit.ly/5d-storage",
     				link_text: "futurism.com"
     			},
     			{
     				header: "WTF - A complete dashboard that runs in the terminal.",
+    				header_icon:"💻",
     				description: " Includes Google calendar, clock, weather, git repos. (It might or might not improve your work flow, but will sure as hell look cool)",
     				link_url: "http://bit.ly/terminal-dashboard",
     				link_text: "wtfutil.com"
     			},
     			{
     				header: "How data storage on DNA works",
+    				header_icon:"⚗",
     				description: "(Yeah...either we are progressing too fast, or you're getting old quick )",
     				link_url: "http://bit.ly/data-dna-storage",
     				link_text: "wolfcast.co.uk"
     			},
     			{
     				header: "DeepMind built an AI that imagines",
+    				header_icon:"🤔",
     				description: "The company’s latest advance that translates 2D pictures to 3D images could lead to machines that can make better sense of a scene",
     				link_url: "http://bit.ly/deepmind-imagines",
     				link_text: "sciencemag.org"
     			},
     			{
     				header: "Carrot-enriched concrete",
+    				header_icon:"🥕",
     				description: "Material scientists are strengthening structures using plant fibres",
     				link_url: "http://bit.ly/plant-cement",
     				link_text: "economist.com"
     			},
     			{
     				header: "Tripling the energy storage of lithium-ion batteries",
+    				header_icon:"🔋",
     				description: "Scientists have synthesized a new cathode material from iron fluoride that surpasses the capacity limits of traditional lithium-ion batteries",
     				link_url: "http://bit.ly/tripling-lithium-storage",
     				link_text: "sciencedaily.com"
@@ -267,18 +283,21 @@ app.controller('MainCtrl', function($scope) {
     		content: [
     			{
     				header: "Chess tournament in Dadar (has an entry fee)",
+    				header_icon:"♟",
     				description: "",
     				link_url: "http://bit.ly/dadar-chess-tournament",
     				link_text: "eventshigh.com"
     			},
     			{
     				header: "Every Noise At Once",
+    				header_icon:"🎶",
     				description: "An interactive website with noises from different musical genres. For the music lover in you",
     				link_url: "http://bit.ly/everynoise-music",
     				link_text: "everynoise.com"
     			},
     			{
     				header: "Make custom designed backgrounds",
+    				header_icon:"⛰",
     				description: "Tired of searching of good background wallpapers for your social media profile/website/blog. Go and make a cool one for yourself.",
     				link_url: "http://bit.ly/cool-backgrounds",
     				link_text: "coolbackgrounds.io"
