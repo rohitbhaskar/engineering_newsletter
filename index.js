50742b5da7469aaa4813b1af9f41abb919b83766
rohitbhaskar <bhaskar.rohit.work@gmail.com>
2018-06-24 13:44:49 +0530

    first attempt at serving a static html file on heroku

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname + '/newsletter')));
+app.use("/styles", express.static(__dirname + '/newsletter/styles'));
+app.use("/images", express.static(__dirname + '/newsletter/images'));
+app.use("/scripts", express.static(__dirname + '/newsletter/scripts'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'newsletter/views/index.html'));
+});
+
+// add other routes below
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname + 'newsletter/views/about.html'));
+});
+
+app.listen(process.env.PORT || 3000);
